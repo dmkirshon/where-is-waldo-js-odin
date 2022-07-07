f4cf9498c0de2850a503d1eb052ad439f28ecae2
dmkirshon <kirshon.david@gmail.com>
2022-07-07 13:58:56 +0200

    feat: Remove unused text from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,7 +35,6 @@ const Header = ({ imageLoaded, gameWon, currentTimerTime }) => {
     <header className="app-header">
       <h1 className="app-header-title">Find the Narwhals</h1>
       <section className="app-header-game-info">
-        <p className="game-info-narwhals-remaining">Narwhal's left</p>
         <p className="game-info-timer">Timer</p>
       </section>
     </header>
